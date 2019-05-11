6c722c62cab7768c313aa345d561c4e520105beb
DESKTOP-PQDEOHM\Hamed <hamed.fatehi.66@gmail.com>
2019-05-11 21:47:12 +0200

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AppPreloadingStrategy } from './app-preloading-strategy';
+import { LoginComponent } from './eager/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'lazy',
+    loadChildren: './lazy/lazy.module#LazyModule',
+  },
+  {
+    path: 'pre',
+    loadChildren: './pre/pre.module#PreModule',
+    data: { preload: true, delay: 2000 },
+  }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    /*
+     The method is called forRoot() because you configure the router at the application's root level.
+     The forRoot() method supplies the service providers and directives needed for routing,
+     and performs the initial navigation based on the current browser URL.
+     https://angular.io/tutorial/toh-pt5
+     */
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: AppPreloadingStrategy
+    }),
+  ],
+  exports: [
+    RouterModule,
+  ],
 })
 export class AppRoutingModule { }
